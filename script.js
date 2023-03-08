fef028380060b894d00c1312317465ee5c638b9a
rnutall <rnutall1@gmail.com>
2023-03-07 22:34:20 -0600

    update

--- script.js
+++ script.js
@@ -1,25 +1,65 @@
-var city = "";
+/* $.ajax({
+    url: 'http://api.weatherstack.com/current',
+    data: {
+      access_key: '',
+      query: '78249',
+      units: 'f'
+    },
+    dataType: 'json',
+    success: function(apiResponse) {
+        console.log(apiResponse);
+        var holdThis = JSON.stringify(apiResponse);
+        localStorage.setItem('holdThis', holdThis)
+      console.log(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`);
+    }
+  }); */
 
-function searchDes(event){
-    //event.preventDefault();
-    city = $("#city").val();
+const knownCities = [];
+
+/* If statement
+Form posting from the page with the city that will be assigned to the variable */
+
+function searchCity(event){
+    event.preventDefault();
+    var city = $('#city').val();
     console.log(city);
 
-    for(i = 0; i < localStorage.length; i++) {
+    // for(i=0; i < localStorage.length; i++){
         
-    }
+    // }
+    
 
     $.ajax({
-        url: "https://home.openweathermap.org/api_keys" 
+        url: 'https://api.openweathermap.org/data/2.5/forecast',
         data: {
-            units: "imperial",
-            apiKey: a0f632a8daa985723cec2b7f8cd3d812
-
+            //lat:44.34,
+            //lon:10.99,
+            q: city,
+            //zip: 78249,
+            units: 'imperial',
+            appid: 'a0f632a8daa985723cec2b7f8cd3d812',
         },
-        dataType: "json",
-        success: function(resposeApi) {
-            var holdThis = JSON.parse
+        dataType: 'json',
+        success: function(apiResponse) {
+         
+    
+            console.log(apiResponse.list[5])
+            $('#icon1').text(apiResponse.list[5].weather.object)
+          $('#date1').text(apiResponse.list[5].dt_txt)
+          $('#temp1').text(apiResponse.list[5].main.temp_max)
+          $('#humidity1').text(apiResponse.list[5].main.humidity)
+          $('#windspeed1').text(apiResponse.list[5].wind.speed)
         }
     })
+
+   
+    
 }
-// apiKey: a0f632a8daa985723cec2b7f8cd3d812
+
+$('#search').on('submit', searchCity)
+
+
+
+
+
+
