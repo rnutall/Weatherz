6804c7ef6222111e29cd913b738c0a00e3c76b56
rnutall <rnutall1@gmail.com>
2023-03-08 02:28:01 -0600

    updated files

--- script.js
+++ script.js
@@ -42,13 +42,43 @@ function searchCity(event){
         dataType: 'json',
         success: function(apiResponse) {
          
-    
-            console.log(apiResponse.list[5])
-            $('#icon1').text(apiResponse.list[5].weather.object)
+          $('#icon0').text(apiResponse.list[5])
+          $('#date0').text(apiResponse.list[5].dt_txt)
+          $('#temp0').text(apiResponse.list[5].main.temp_max)
+          $('#humidity0').text(apiResponse.list[5].main.humidity)
+          $('#windspeed0').text(apiResponse.list[5].wind.speed)
+
+           // console.log(apiResponse.list[5])
+          $('#icon1').text(apiResponse.list[5].weather)
           $('#date1').text(apiResponse.list[5].dt_txt)
           $('#temp1').text(apiResponse.list[5].main.temp_max)
           $('#humidity1').text(apiResponse.list[5].main.humidity)
           $('#windspeed1').text(apiResponse.list[5].wind.speed)
+          
+          console.log(apiResponse.list)
+          $('#icon2').text(apiResponse.list[13])
+          $('#date2').text(apiResponse.list[13].dt_txt)
+          $('#temp2').text(apiResponse.list[13].main.temp_max)
+          $('#humidity2').text(apiResponse.list[13].main.humidity)
+          $('#windspeed2').text(apiResponse.list[13].wind.speed)
+
+          $('#icon3').text(apiResponse.list[21])
+          $('#date3').text(apiResponse.list[21].dt_txt)
+          $('#temp3').text(apiResponse.list[21].main.temp_max)
+          $('#humidity3').text(apiResponse.list[21].main.humidity)
+          $('#windspeed3').text(apiResponse.list[21].wind.speed)
+
+          $('#icon4').text(apiResponse.list[29])
+          $('#date4').text(apiResponse.list[29].dt_txt)
+          $('#temp4').text(apiResponse.list[29].main.temp_max)
+          $('#humidity4').text(apiResponse.list[29].main.humidity)
+          $('#windspeed4').text(apiResponse.list[29].wind.speed)
+
+          $('#icon5').text(apiResponse.list[37])
+          $('#date5').text(apiResponse.list[37].dt_txt)
+          $('#temp5').text(apiResponse.list[37].main.temp_max)
+          $('#humidity5').text(apiResponse.list[37].main.humidity)
+          $('#windspeed5').text(apiResponse.list[37].wind.speed)
         }
     })
 
